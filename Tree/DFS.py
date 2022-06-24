b1b842cb872b71fe9c06c538f33245fc93ac8712
Diwakar Sharma <diwakarsharma4949@gmail.com>
2022-06-24 16:21:33 +0530

    DFS recursive + iterative

--- Tree/DFS.py
+++ Tree/DFS.py
@@ -31,3 +31,43 @@ dfs(root)
 
 #OUTPUT--> 8 4 9 2 10 5 1 6 3 7 
  
+#------------------------------------------------------------------------
+
+#DFS iterative approach using stack.
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+            
+def construct_tree(lst, i, n):
+    root = None
+        
+    if i < n:
+        root = Node(lst[i])
+            
+        root.left = construct_tree(lst, 2*i+1, n)
+        root.right = construct_tree(lst, 2*i+2, n)
+            
+    return root
+ 
+def dfs(root):
+    if root:
+        stack = []
+        current_node = root
+        while stack or current_node:
+            if current_node:
+                stack.append(current_node)
+                current_node = current_node.left
+            else:
+                current_node = stack.pop()
+                print(current_node.value, end = " ")
+                current_node = current_node.right
+ 
+lst = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10]
+n = len(lst)
+root = construct_tree(lst, 0, n)
+dfs(root)
+
+#OUTPUT--> 8 4 9 2 10 5 1 6 3 7 
